02f69c09389dcbc37e449ce046a0ea0fcc0f884d
Victor Pereira <victor.cordeiro2@gmail.com>
2020-06-21 18:49:14 -0300

    Spring Security configuration

---
+++ server/mymarketplace/src/main/java/com/victorpereira/mymarketplace/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.victorpereira.mymarketplace.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private Environment env;
+
+	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
+
+	private static final String[] PUBLIC_MATCHERS_GET = { "/products/**", "/categories/**" };
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		http.cors().and().csrf().disable();
+		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll()
+				.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().anyRequest().authenticated();
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+}
--- server/mymarketplace/src/main/java/com/victorpereira/mymarketplace/config/TestConfig.java
+++ server/mymarketplace/src/main/java/com/victorpereira/mymarketplace/config/TestConfig.java
@@ -1,11 +1,18 @@
 package com.victorpereira.mymarketplace.config;
 
+import java.io.InputStream;
 import java.text.ParseException;
+
+import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 
 import com.victorpereira.mymarketplace.services.DBService;
 import com.victorpereira.mymarketplace.services.EmailService;
@@ -28,4 +35,45 @@ public class TestConfig {
 	public EmailService emailService() {
 		return new MockEmailService();
 	}
+	
+	@Bean
+	public JavaMailSender javaMailSender() { 
+        return new JavaMailSender() {
+			
+			@Override
+			public void send(SimpleMailMessage... simpleMessages) throws MailException {
+			}
+			
+			@Override
+			public void send(SimpleMailMessage simpleMessage) throws MailException {
+			}
+			
+			@Override
+			public void send(MimeMessagePreparator... mimeMessagePreparators) throws MailException {
+			}
+			
+			@Override
+			public void send(MimeMessagePreparator mimeMessagePreparator) throws MailException {
+			}
+			
+			@Override
+			public void send(MimeMessage... mimeMessages) throws MailException {
+			}
+			
+			@Override
+			public void send(MimeMessage mimeMessage) throws MailException {
+			}
+			
+			@Override
+			public MimeMessage createMimeMessage(InputStream contentStream) throws MailException {
+				return null;
+			}
+			
+			@Override
+			public MimeMessage createMimeMessage() {
+				return null;
+			}
+		};
+  } 
+	
 }
