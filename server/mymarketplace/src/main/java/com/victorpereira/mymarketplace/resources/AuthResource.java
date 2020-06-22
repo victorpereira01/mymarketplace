d8ba7660119e84baa18136fdeda6146c87dff3bb
Victor Pereira <victor.cordeiro2@gmail.com>
2020-06-22 20:31:57 -0300

    Refresh token

---
+++ server/mymarketplace/src/main/java/com/victorpereira/mymarketplace/resources/AuthResource.java
@@ -0,0 +1,29 @@
+package com.victorpereira.mymarketplace.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.victorpereira.mymarketplace.security.JWTUtil;
+import com.victorpereira.mymarketplace.security.UserSS;
+import com.victorpereira.mymarketplace.services.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@PostMapping(value = "/refresh_token")
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+}
--- server/mymarketplace/src/main/java/com/victorpereira/mymarketplace/resources/OrderResource.java
+++ server/mymarketplace/src/main/java/com/victorpereira/mymarketplace/resources/OrderResource.java
@@ -1,7 +1,6 @@
 package com.victorpereira.mymarketplace.resources;
 
 import java.net.URI;
-import java.util.List;
 
 import javax.validation.Valid;
 
