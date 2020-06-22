3ab49056549721071c3177ca834cd4e6270a374e
Victor Pereira <victor.cordeiro2@gmail.com>
2020-06-22 19:43:49 -0300

    Authorization implemented

--- server/mymarketplace/src/main/java/com/victorpereira/mymarketplace/config/SecurityConfig.java
+++ server/mymarketplace/src/main/java/com/victorpereira/mymarketplace/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.victorpereira.mymarketplace.security.JWTAuthenticationFilter;
+import com.victorpereira.mymarketplace.security.JWTAuthorizationFilter;
 import com.victorpereira.mymarketplace.security.JWTUtil;
 
 @Configuration
@@ -49,6 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 				.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
