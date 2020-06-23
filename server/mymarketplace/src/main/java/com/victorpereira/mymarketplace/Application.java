124a33ff3b1843707e05e2e37ec431df7ba5ed50
Victor Pereira <victor.cordeiro2@gmail.com>
2020-06-22 22:24:08 -0300

    Saving file on S3 + .gitignore

--- server/mymarketplace/src/main/java/com/victorpereira/mymarketplace/Application.java
+++ server/mymarketplace/src/main/java/com/victorpereira/mymarketplace/Application.java
@@ -1,11 +1,17 @@
 package com.victorpereira.mymarketplace;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.victorpereira.mymarketplace.services.S3Service;
+
 @SpringBootApplication
 public class Application implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -13,5 +19,6 @@ public class Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\Users\\victo\\Pictures\\Camera Roll\\groot.jpg");
 	}
 }
