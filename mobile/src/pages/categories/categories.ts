97250ec153d3f35335684f71e09800727d6016bf
Victor Pereira <victor.cordeiro2@gmail.com>
2020-06-25 20:04:00 -0300

    Listing categories

--- mobile/src/pages/categories/categories.ts
+++ mobile/src/pages/categories/categories.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CategoryService } from '../../services/domain/category.service';
 
 @IonicPage()
 @Component({
@@ -8,11 +9,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CategoriesPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public categoryService: CategoryService) {
   }
+
+
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CategoriesPage');
+    this.categoryService.findAll()
+      .subscribe(response => {
+        console.log(response);
+      },
+        error => {
+          console.log(error)
+        });
   }
-
 }
