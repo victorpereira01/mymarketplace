4540f3d015a052a38e62a291f38a719eeec59658
Victor Pereira <victor.cordeiro2@gmail.com>
2020-06-24 22:17:48 -0300

    Menu on the home screen disabled

--- mobile/src/pages/home/home.ts
+++ mobile/src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, IonicPage } from 'ionic-angular';
+import { NavController, IonicPage, MenuController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -8,10 +8,18 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public menu: MenuController) {
 
   }
 
+  ionViewWillEnter() {
+    this.menu.swipeEnable(false);
+  }
+  
+  ionViewDidLeave() {
+    this.menu.swipeEnable(true);
+  }
+  
   login() {
     this.navCtrl.setRoot('CategoriesPage');
   }
