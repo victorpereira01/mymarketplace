f622e6e24350545cdb9beee0c57b7409bb7a223f
Victor Pereira <victor.cordeiro2@gmail.com>
2020-06-25 23:23:35 -0300

    Showing categories

--- mobile/src/config/api.config.ts
+++ mobile/src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080"
+    baseUrl: "http://localhost:8080",
+    bucketBaseUrl: "https://mymarketplace-sb.s3-sa-east-1.amazonaws.com/"
 }
--- mobile/src/pages/categories/categories.ts
+++ mobile/src/pages/categories/categories.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CategoryService } from '../../services/domain/category.service';
+import { CategoryDTO } from '../../models/category.dto';
+import { API_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
@@ -9,18 +11,20 @@ import { CategoryService } from '../../services/domain/category.service';
 })
 export class CategoriesPage {
 
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+
+  items: CategoryDTO[];
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public categoryService: CategoryService) {
   }
 
-
-
   ionViewDidLoad() {
     this.categoryService.findAll()
       .subscribe(response => {
-        console.log(response);
+        this.items = response;
       },
         error => {
           console.log(error)
