29760a70601481ede9fdfc3e27bc00a556214907
Victor Pereira <victor.cordeiro2@gmail.com>
2020-06-26 22:31:57 -0300

    Profile page

--- mobile/src/app/app.component.ts
+++ mobile/src/app/app.component.ts
@@ -19,7 +19,8 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: 'HomePage' }
+      { title: 'Perfil', component: 'ProfilePage' },
+      { title: 'Categorias', component: 'CategoriesPage' },
     ];
 
   }
