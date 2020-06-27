ed08f8ed54dd7f3512995375e1a614f18ba3d574
Victor Pereira <victor.cordeiro2@gmail.com>
2020-06-27 18:57:40 -0300

    formGroup controlling signup form

--- mobile/src/pages/signup/signup.ts
+++ mobile/src/pages/signup/signup.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -8,10 +9,32 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SignupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  formGroup: FormGroup;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public formBuilder: FormBuilder) {
+    this.formGroup = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
+      email: ['', [Validators.required, Validators.email]],
+      type: ['', [Validators.required]],
+      cpfOuCnpj: ['', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
+      password: ['', [Validators.required]],
+      publicPlace: ['', [Validators.required]],
+      number: ['', [Validators.required]],
+      complement: ['', []],
+      neighborhood: ['', []],
+      cep: ['', [Validators.required]],
+      tel1: ['', [Validators.required]],
+      tel2: ['', []],
+      tel3: ['', []],
+      stateId: [null, [Validators.required]],
+      cityId: [null, [Validators.required]]
+    });
   }
 
   signupUser() {
-    
+
   }
 }
