79c921675c1a73a1aac9ba0ba2c514c5b8a44c96
Victor Pereira <victor.cordeiro2@gmail.com>
2020-06-30 21:24:42 -0300

    Documentation using Swagger

--- server/src/main/java/com/victorpereira/mymarketplace/config/SecurityConfig.java
+++ server/src/main/java/com/victorpereira/mymarketplace/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -42,6 +43,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private static final String[] PUBLIC_MATCHERS_GET = { "/products/**", "/categories/**", "/states/**" };
 
 	private static final String[] PUBLIC_MATCHERS_POST = { "/clients", "/auth/forgot/**" };
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**",
+				"/configuration/**", "/swagger-ui.html", "/webjars/**");
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
